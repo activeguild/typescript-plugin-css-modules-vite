af403beadf5346931e43396f7538f69e03215321
j1ngzoue <workdrive0306@gmail.com>
2021-11-23 14:32:01 +0900

    Remove unnesesary codes

--- src/format.ts
+++ src/format.ts
@@ -1,15 +1,10 @@
 export const formatClassNames = (classNameKeys: Map<string, boolean>) => {
-  let exportTypes = "",
-    exportClassNames = "export type ClassNames = ";
+  let exportTypes = "";
   const exportStyle = "export default classNames;";
   for (const classNameKey of classNameKeys.keys()) {
     exportTypes = `${exportTypes}\n${formatExportType(classNameKey)}`;
-    exportClassNames =
-      exportClassNames !== "export type ClassNames = "
-        ? `${exportClassNames} | '${classNameKey}'`
-        : `${exportClassNames} '${classNameKey}'`;
   }
 
-  return `declare const classNames: {${exportTypes}\n};\n${exportStyle}\n${exportClassNames}`;
+  return `declare const classNames: {${exportTypes}\n};\n${exportStyle}`;
 };
 const formatExportType = (key: string) => `  readonly '${key}': '${key}';`;
